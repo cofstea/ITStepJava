96ae23cb679b7137a1a26533640d0287b11bb205
Bivol Constantin <Constantin.Bivol@endava.com>
2020-06-23 20:38:10 +0300

    Alex Task

---
+++ src/com/itstep/lesson/_19/AlexClass.java
@@ -0,0 +1,9 @@
+package com.itstep.lesson._19;
+
+public class AlexClass {
+
+    public static void main(String[] args) {
+        System.out.println("Alex has done the Task for lesson " + 19);
+    }
+
+}
