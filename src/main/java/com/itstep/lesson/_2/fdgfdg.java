4df4202b87732c3cd3ab0083e2721937a60e2cbd
Constantin Bivol <Constantin.Bivol@endava.com>
2021-03-29 15:10:26 +0300

    Task1 Done Nina

--- src/main/java/com/itstep/lesson/_2/fdgfdg.java
+++ src/main/java/com/itstep/lesson/_2/fdgfdg.java
@@ -1,8 +1,11 @@
 package com.itstep.lesson._2;
 
 public class fdgfdg {
+    public static final String NEW_FIELDS = "Hello there is my first task!";
 
     public static void main(String[] args) {
-        System.out.println("hello World, That is my first task!");
+        System.out.println(NEW_FIELDS);
     }
+
+
 }
